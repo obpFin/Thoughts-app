eae8692345a88456ec1496ea27e4089d3f85ea7b
Oskari Peltonen <oskari.peltonen@outlook.com>
2017-09-28 21:20:13 +0300

    add schema for user model

---
+++ server/models/user.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+// const jwt = require('jsonwebtoken');
+const _ = require('lodash');
+
+
+var UserSchema = new mongoose.Schema({
+	email: {
+		type: String,
+		required: true,
+		minlength: 1,
+		trim: true,
+		unique: true,
+		validate: {
+			isAsync: false,
+			validator: validator.isEmail,
+			message: '{VALUE} is not a valid email'
+		}
+	},
+	password: {
+		type: String,
+		required: true,
+		minlength: 5
+	},
+	tokens: [{
+		access: {
+			type: String,
+			required: true
+		},
+		token: {
+			type: String,
+			required: true
+		}
+	}]
+});
