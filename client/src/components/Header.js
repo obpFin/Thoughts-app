d8a117e4209f99fa51a44b3958b8ef102d07bf3c
Oskari Peltonen <oskari.peltonen@outlook.com>
2017-10-03 21:14:26 +0300

    added HeaderMenu

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
+import HeaderMenu from './HeaderMenu'
+
 const Header = (props) => {
     return (
-      <div className="header">
-        <h1>{props.title}</h1>
-      </div>
+    	<table>
+    		<tbody>
+      		<tr className="header">
+	      		<td className="title">
+	      			<h1 >{props.title}</h1>
+	      		</td>
+	      		<td>
+	        		<i className="material-icons md-24">chat_bubble</i>
+	      		</td>
+	      		<HeaderMenu />
+      		</tr>
+    		</tbody>
+    	</table>
     );
   };
 
---
+++ client/src/components/HeaderMenu.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const HeaderMenu = (props) => {
+    return (
+			<td className="upper-menu">
+	  		<i className="material-icons md-32">add_circle</i>
+	  		<i className="material-icons md-32">account_circle</i>
+	  		<i className="material-icons md-32">home</i>
+	  		<i className="material-icons md-32 status">power_settings_new</i>
+
+			</td>
+    );
+  };
+
+export default HeaderMenu;
