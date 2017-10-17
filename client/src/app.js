174287ed40ab3b17e7fe7192e524bc8a3a8c8a04
Oskari Peltonen <oskari.peltonen@outlook.com>
2017-10-17 17:24:34 +0300

    added normalize.css

--- client/src/app.js
+++ client/src/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ThoughtsApp from './components/ThoughtsApp';
+import 'normalize.css/normalize.css'
 import css from './styles/app.scss';
 
 ReactDOM.render(<ThoughtsApp />, document.getElementById('app'));
