be8d89cf64883174105d59b3a05d7bd62f698492
Oskari Peltonen <oskari.peltonen@outlook.com>
2017-10-26 14:19:23 +0300

    added styling to Modal component

--- client/src/components/AddThoughtModal.js
+++ client/src/components/AddThoughtModal.js
@@ -9,7 +9,11 @@ const AddThoughtModal = (props) => (
       closeTimeoutMS={200}
       className="modal"
   >
-      <h3>Under construction</h3>
+      <h3 className="modal-title">Share a thought!</h3>
+      <form>
+			  <input className="text-area" type="text" name="name" />
+			  <input className="submit-thought" type="submit" value="Submit" />
+			</form>
   </Modal>
 );
 
