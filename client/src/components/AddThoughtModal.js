8f3e1e599b7c3f3eab242e4eadb2eda6ff827e28
Oskari Peltonen <oskari.peltonen@outlook.com>
2017-10-26 17:13:04 +0300

    modified Modal component styling

--- client/src/components/AddThoughtModal.js
+++ client/src/components/AddThoughtModal.js
@@ -1,20 +1,48 @@
 import React from 'react';
 import Modal from 'react-modal';
+import axios from 'axios';
 
-const AddThoughtModal = (props) => (
-  <Modal
-      isOpen={props.isOpen}
-      contentLabel="add a Thought"
-      onRequestClose={props.handleToggleThought}
-      closeTimeoutMS={200}
-      className="modal"
-  >
-      <h3 className="modal-title">Share a thought!</h3>
-      <form>
-			  <input className="text-area" type="text" name="name" />
-			  <input className="submit-thought" type="submit" value="Submit" />
-			</form>
-  </Modal>
-);
+import { apiUrl } from './../utils/utils';
 
-export default AddThoughtModal;
+export default class AddThoughtModal extends React.Component {
+
+	handleSubmit = (event) => {
+		event.preventDefault();
+		let inputValue = this.textInput.value
+		if (inputValue) {
+			console.log();
+			axios.post(`${apiUrl}/thoughts`, {
+				text:inputValue
+			})
+	    .then(function (response) {
+	    	console.log(response);
+	    })
+	    .catch(function (error) {
+	      console.log(error);
+	    });
+		}
+	};
+
+	render() {
+		return (
+			<Modal
+	      isOpen={this.props.isOpen}
+	      contentLabel="add a Thought"
+	      onRequestClose={this.props.handleToggleThought}
+	      closeTimeoutMS={200}
+	      className="modal"
+	  	>
+	      <h3 className="modal-title">Share a thought!</h3>
+	      <form onSubmit={this.handleSubmit}>
+				  <input className="text-area" 
+				  	type="text" 
+				  	name="name" 
+				  	ref={(input) => { this.textInput = input; }}
+				  />
+				  <input className="submit-thought" type="submit" value="Submit" />
+				</form>
+	  </Modal>
+	 );
+	}
+  
+};
