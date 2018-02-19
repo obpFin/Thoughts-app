a6353ad294d72d502ca444bbbfa0f75a85faeaf6
Oskari Peltonen <oskari.peltonen@outlook.com>
2018-02-19 17:05:58 +0200

    added thoughts selector

---
+++ client/src/selectors/thoughts.js
@@ -0,0 +1,17 @@
+import moment from 'moment';
+
+// Get visible thoughts
+export default (thoughts, { text, sortBy, startDate, endDate }) => {
+  return thoughts.filter((thought) => {
+    const createdAtMoment = moment(thought.createdAt);
+    const startDateMatch = startDate ? startDate.isSameOrBefore(createdAtMoment, 'day') : true;
+    const endDateMatch = endDate ? endDate.isSameOrAfter(createdAtMoment, 'day') : true;
+    const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
+
+    return startDateMatch && endDateMatch && textMatch;
+  }).sort((a,b) => {
+    if (sortBy === 'date') {
+      return a.createdAt < b.createdAt ? 1 : -1 
+    } 
+  });
+};
