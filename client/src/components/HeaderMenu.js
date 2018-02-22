92d7db186e9ea712915e15a6e7f7d56b545f7e73
Oskari Peltonen <oskari.peltonen@outlook.com>
2018-02-22 18:38:17 +0200

    header position, center thoughts

--- client/src/components/HeaderMenu.js
+++ client/src/components/HeaderMenu.js
@@ -4,20 +4,20 @@ import DropDown from './HeaderDropdown'
 
 const HeaderMenu = (props) => {
     return (
-				<td className="upper-menu">
-					{props.showButtons &&
-						<div>
-				  		<a onClick={props.handleToggleThought} className="add-thought material-icons md-32">add_circle</a>
-				  		<a onClick={props.handleToggleProfile} className="account material-icons md-32">account_circle</a>
-				  		<a onClick={props.handleToggleSettings} className="settings material-icons md-32">power_settings_new</a>
-							<AddThoughtModal 
-								isOpen = {props.addThoughtOpen} 
-								handleToggleThought = {props.handleToggleThought}
-								getThoughts = {props.getThoughts}
-							/>
-						</div>
-					}
-				</td>
+			<td className="upper-menu">
+				{props.showButtons &&
+					<div>
+			  		<a onClick={props.handleToggleThought} className="add-thought material-icons md-32">add_circle</a>
+			  		<a onClick={props.handleToggleProfile} className="account material-icons md-32">account_circle</a>
+			  		<a onClick={props.handleToggleSettings} className="settings material-icons md-32">power_settings_new</a>
+						<AddThoughtModal 
+							isOpen = {props.addThoughtOpen} 
+							handleToggleThought = {props.handleToggleThought}
+							getThoughts = {props.getThoughts}
+						/>
+					</div>
+				}
+			</td>
     );
   };
 
