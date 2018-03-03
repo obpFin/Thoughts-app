25027bdb2e742277c2b561e574675b8ddbe372ac
Oskari Peltonen <oskari.peltonen@outlook.com>
2018-03-03 15:11:03 +0200

    ProfileInfo styling

--- client/src/components/ProfileInfo.js
+++ client/src/components/ProfileInfo.js
@@ -2,19 +2,51 @@ import React from 'react';
 import { withRouter } from "react-router-dom";
 
  class ProfileInfo extends React.Component {
+	 state = {
+		 user: {
+			userName: this.props.user.userName || '',
+			email: this.props.user.email || ''
+		 },
+		 editUserName: false,
+		 editEmail: false
+	 }
 	pushBack = () => {
     this.props.history.push('/');
 	};
+	setUser = (user) => {
+		if (user) {
+			this.setState({
+				user
+			});
+		}
+	}
+	componentDidMount() {
+	}
+	componentWillReceiveProps(nextProps) {
+		if (nextProps.user != this.state.user) {
+			this.setUser(nextProps.user);
+		}
+	}
 	render() {
-		const { userName, email } = this.props.user;
+		const { userName, email } = this.state.user;
 		return (
 			<div className = "profile info-container">
 				<a onClick={this.pushBack} className="material-icons blue md-36">arrow_back</a>
 				<div>
-					<p className="darken blue">Usename</p>
-					<p className="profile node">{userName} <span className="blue edit">edit</span></p>
-					<p className="darken blue">Email</p>
-					<p className="profile node">{email} <span className="blue edit">edit</span></p>
+					<div className="info cell">
+						<p className="darken blue">Usename</p>
+						<div>
+							<input type="text" value={userName} onChange={this.handleChange} />
+							<i>edit</i>
+						</div>
+					</div>
+					<div className="info cell">
+						<p className="darken blue">Email</p>
+						<div>
+							<input type="text" value={email} onChange={this.handleChange} />
+							<i>edit</i>
+						</div>
+					</div>
 				</div>
 			</div>
 		);
