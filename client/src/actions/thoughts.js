e56e63faee96ab9056d3b0a8edd3d271124a326d
Oskari Peltonen <oskari.peltonen@outlook.com>
2018-03-26 21:52:15 +0300

    fetchAllThoughts action + reducer

--- client/src/actions/thoughts.js
+++ client/src/actions/thoughts.js
@@ -1,16 +1,60 @@
+import { apiUrl } from './../utils/utils';
+
+export const FETCH_THOUGHTS_BEGIN = 'FETCH_THOUGHTS_BEGIN';
+export const FETCH_THOUGHTS_SUCCESS = 'FETCH_THOUGHTS_SUCCESS';
+export const FETCH_THOUGHTS_FAILURE = 'FETCH_THOUGHTS_FAILURE';
+export const ADD_THOUGHT = 'ADD_THOUGHT';
+export const EDIT_THOUGHT = 'EDIT_THOUGHT';
+export const REMOVE_THOUGHT = 'REMOVE_THOUGHT';
+
+export const fetchAllThoughts = () => {
+	return dispatch => {
+			dispatch(fetchThoughtsBegin());
+		return fetch(`${apiUrl}/thoughts/all`)
+			.then(handleErrors)
+			.then((res => res.json()))
+			.then(json => {
+				dispatch(fetchThoughtsSuccess(json.thoughts));
+				return json.thoughts;
+			})
+			.catch(error => dispatch(fetchThoughtsError(error)));
+	};
+};
+
+const handleErrors = (response) => {
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  return response;
+};
+
+export const fetchThoughtsBegin = () => ({
+  type: FETCH_THOUGHTS_BEGIN
+});
+
+export const fetchThoughtsSuccess = thoughts => ({
+  type: FETCH_THOUGHTS_SUCCESS,
+  payload: { thoughts }
+});
+
+export const fetchThoughtsError = error => ({
+  type: FETCH_THOUGHTS_FAILURE,
+  payload: { error }
+});
+
 export const addThought = (
 	{
 		text = ''
 	} = {}
 ) => ({
-	type: 'ADD_THOUGHT',
+	type: ADD_THOUGHT,
 	thought: {
 		text
 	}
 });
 
 export const editThought = (id, updates) => ({
-	type: 'EDIT_THOUGHT',
+	type: EDIT_THOUGHT,
 	updates
 });
 
--- client/src/reducers/thoughts.js
+++ client/src/reducers/thoughts.js
@@ -1,15 +1,48 @@
-const thoughtsReducerDefaultState = [];
+import {
+  FETCH_THOUGHTS_BEGIN,
+  FETCH_THOUGHTS_SUCCESS,
+  FETCH_THOUGHTS_FAILURE,
+  ADD_THOUGHT,
+  EDIT_THOUGHT,
+  REMOVE_THOUGHT
 
-export default (state = thoughtsReducerDefaultState, action) => {
+} from '../actions/thoughts';
+
+const initialState = {
+  thoughts: [],
+  loading: false,
+  error: null
+};
+
+export default (state = initialState, action) => {
   switch (action.type) {
-    case 'ADD_THOUGHT':
+    case FETCH_THOUGHTS_BEGIN:
+      return {
+        ...state,
+        loading: true,
+        error: null
+      };
+    case FETCH_THOUGHTS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        thoughts: action.payload.thoughts
+      };
+    case FETCH_THOUGHTS_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload.error,
+        thoughts: []
+      };
+    case ADD_THOUGHT:
       return [
         ...state,
         action.thought
       ];
-    case 'REMOVE_THOUGHT':
+    case REMOVE_THOUGHT:
       return state.filter(({ id }) => id !== action.id);
-    case 'EDIT_THOUGHT':
+    case EDIT_THOUGHT:
       return state.map((thought) => {
         if (thought.id === action.id) {
           return {
